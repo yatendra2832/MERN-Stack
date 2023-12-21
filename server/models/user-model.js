c942a9c5cf50f07f9eeeccc40d7ab172ff693699
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-21 11:40:00 +0530

    creating user model for the user collection

---
+++ server/models/user-model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+// define the model or the collection name
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
