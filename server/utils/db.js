c129c9472029402c356279dc2c73be3e0624c636
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-21 11:30:05 +0530

    Securing our password and credentials using dotenv

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost:27017/mern_admin';
+// const URI = 'mongodb://localhost:27017/mern_admin';
+const URI = process.env.MONGODB_URI;
+
 const connectDB = async () => {
     try {
         await mongoose.connect(URI);
