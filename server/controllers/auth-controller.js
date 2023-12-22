ce94725a15cdca6a7bd84e997a76eed01890420c
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-22 22:45:10 +0530

    Build a complete user login Route with functionality

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -43,4 +43,34 @@ const register = async (req, res) => {
     }
 }
 
-module.exports = { home, register }
+// *--------------------------------------------
+// User Login Logic
+// *--------------------------------------------
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const userExist = await User.findOne({ email });
+
+        if (!userExist) {
+            return res.status(400).json({ msg: "User does not exist" });
+        }
+
+        const user = await bcrypt.compare(password, userExist.password);
+
+        if (user) {
+            res.status(200).json({
+                msg: "Login Successful",
+                token: await userExist.generateToken(),
+                userId: userExist._id.toString()
+            })
+        } else {
+            res.status(401).json({ msg: "Invalid Credentials" });
+        }
+
+    } catch (error) {
+        res.status(500).json("Internal Server Error");
+    }
+}
+
+module.exports = { home, register, login }
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authcontroller = require('../controllers/auth-controller');
+
 router.route('/').get(authcontroller.home)
 router.route('/register').post(authcontroller.register)
+router.route('/login').post(authcontroller.login)
 
 module.exports = router;
-/*
-HTTP methods and their meaning
-GET -> Read data
-POST -> insert data
-PUT or PATCH -> update data
-DELETE -> delete data
-*/
+
