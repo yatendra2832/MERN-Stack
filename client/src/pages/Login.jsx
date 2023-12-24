1c74d2b538bb4866da0dfdfbf808d50058a99c63
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-24 22:16:12 +0530

    Login Form UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,80 @@
-import React from 'react'
+import React ,{useState} from 'react'
 
 const Login = () => {
+  const[user,setUser] = useState({
+    email:"",
+    password:""
+  })
+
+  // Handling input values
+  const handleInputs = (e) =>{
+    const name = e.target.name
+    const value = e.target.value
+    setUser({...user,[name]:value})
+  }
+
+  // Hanlding form submission
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(user);
+  }
+
   return (
-    <div><h1>Hello Login Page</h1></div>
+    <section>
+    <main>
+      <div className="section-login">
+        <div className="container grid grid-two-cols">
+          <div className="login-image">
+            <img src="/images/login.png" alt="login img" 
+            width={"400"}
+            height={"400"	}/>
+          </div>
+
+          {/* Login form */}
+          <div className="Login-form">
+            <h1 className='main-heading mb-3'>Login Forn</h1>
+            <br />
+
+            <form  onSubmit={handleSubmit}>
+              
+              <div>
+                <label htmlFor="email">Email</label>
+                <input 
+                type="email"
+                name='email'
+                id='email'
+                placeholder='Enter email'
+                required
+                autoComplete='off'
+                value={user.email}
+                onChange={handleInputs}
+                />
+              </div>
+              <div>
+                <label htmlFor="password">password</label>
+                <input 
+                type="password"
+                name='password'
+                id='password'
+                placeholder='Enter password'
+                required
+                autoComplete='off'
+                value={user.password}
+                onChange={handleInputs}
+                />
+              </div>
+
+              <br />
+              <button type='submit' className='btn btn-submit'>Login Now</button>
+            </form>
+
+          </div>
+
+
+        </div>
+      </div>
+    </main>
+  </section>
   )
 }
 
