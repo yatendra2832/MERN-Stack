dfeb121cb3d390faf8b803437f616b568d4039d1
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-25 14:20:29 +0530

    footer for the website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,30 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Service from './pages/Service';
-import Register from './pages/Register';
-import Login from './pages/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Service from "./pages/Service";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-const App = () =>{
-  return <>
-  <BrowserRouter>
-  <Navbar/>
-  <Routes>
-    <Route path='/' element={<Home />}/>
-    <Route path='/about' element={<About />}/>
-    <Route path='/contact' element={<Contact />}/>
-    <Route path='/service' element={<Service />}/>
-    <Route path='/register' element={<Register />}/>
-    <Route path='/login' element={<Login />}/>
-  </Routes>
-  </BrowserRouter>
-  </>
-}
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+};
 export default App;
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <p className="company-name">yadavjiediting</p>
+      <p>Copyright © 2023. All rights reserved.</p>
+      <address>
+        A-100 Noida, India
+        <br />
+        Phone: (555) 123-4567
+        <br />
+        Email: info@yadavjiediting.com
+      </address>
+    </footer>
+  );
+};
+
+export default Footer;
