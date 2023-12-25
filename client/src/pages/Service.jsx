1745b8ef8f0879d077e3c6135edb95214aaf70cb
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-25 17:33:07 +0530

    created  service page with responsiveness

--- client/src/pages/Service.jsx
+++ client/src/pages/Service.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
 
 const Service = () => {
   return (
-    <div><h1>Hello Service Page</h1></div>
-  )
-}
+    <>
+      {/* Service Section */}
+      <section>
+        <div className="container grid grid-three-cols">
+          <div className="box1">
+            <div className="hero-image">
+              <img
+                src="/images/services.png"
+                alt="digital marketing"
+                width={"300"}
+                height={"300"}
+                className="services-img"
+              />
+            </div>
+            <h2 className="service-header">Web Development</h2>
+            <p className="service-para">
+              We offer professional web development services tailored to your
+              business needs. Our team of experts will build responsive and
+              user-friendly websites.
+            </p>
+          </div>
 
-export default Service
+          <div className="box1">
+            <div className="hero-image">
+              <img
+                src="/images/seo.png"
+                alt="graphic design"
+                width={"300"}
+                height={"300"}
+                className="services-img"
+              />
+            </div>
+            <h2 className="service-header">Graphic Design</h2>
+            <p className="service-para">
+              Need eye-catching visuals for your brand? Our graphic design
+              services cover everything from logos and branding to marketing
+              materials.
+            </p>
+          </div>
+
+          <div className="box1">
+            <div className="hero-image">
+              <img
+                src="/images/network.png"
+                alt="digital marketing"
+                width={"300"}
+                height={"300"}
+                className="services-img"
+              />
+            </div>
+            <h2 className="service-header">Digital Marketing</h2>
+            <p className="service-para">
+              Enhance your online presence with our digital marketing solutions.
+              From social media management to SEO, we've got you covered.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Service;
