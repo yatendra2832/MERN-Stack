40abecfd9d38f79413f8a56881b360c069b645f8
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-25 14:34:50 +0530

    about page created

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div><h1>Hello About Page</h1></div>
-  )
-}
+    <>
+      <section className="section-hero">
+        <div className="container grid grid-two-cols">
+          <div className="hero-content">
+            <p>Welcome , Yatendra Singh</p>
+            <h1>Why Choose YadavjiEditing ?</h1>
+            <p>
+              Expertise: Our team consist of experienced IT professionals who
+              are passionate about staying up-to-date with the latest industry
+              trends.
+            </p>
 
-export default About
+            <p>
+              Customer Focus: We are committed to delivering exceptional
+              customer service and support.
+            </p>
+            <p>
+              Quality: We are committed to delivering high-quality work that
+              meets our clients' needs.
+            </p>
+            <p>
+              Customer Satisfaction: We are committed to delivering exceptional
+              customer service and support.
+            </p>
+            <div className="btn btn-group">
+              <a href="/contact">
+                <button className="btn">Connect Now</button>
+              </a>
+              <a href="/service">
+                <button className="btn secondary-button">Learn More</button>
+              </a>
+            </div>
+          </div>
+
+          <div className="hero-image">
+            <img
+              src="/images/about.png"
+              alt="about image"
+              width={"400"}
+              height={"400"}
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* 2nd section*/}
+      <section className="section-analytics">
+        <div className="container grid grid-four-cols">
+          <div className="div1">
+            <h2>50+</h2>
+            <p>Companies Registered</p>
+          </div>
+          <div className="div1">
+            <h2>150+</h2>
+            <p>Project Done</p>
+          </div>
+          <div className="div1">
+            <h2>250+ </h2>
+            <p>Happy Clients</p>
+          </div>
+          <div className="div1">
+            <h2>650K+</h2>
+            <p>Youtube Subscribers</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
