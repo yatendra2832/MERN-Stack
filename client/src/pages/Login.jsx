9e6fda8bf0edff35fa064a032f3de4f1a6f8111e
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-25 15:12:09 +0530

    Login form completion with the formatting

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,81 +1,82 @@
-import React ,{useState} from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  const[user,setUser] = useState({
-    email:"",
-    password:""
-  })
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
   // Handling input values
-  const handleInputs = (e) =>{
-    const name = e.target.name
-    const value = e.target.value
-    setUser({...user,[name]:value})
-  }
+  const handleInputs = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUser({ ...user, [name]: value });
+  };
 
   // Hanlding form submission
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(user);
-  }
+  };
 
   return (
     <section>
-    <main>
-      <div className="section-login">
-        <div className="container grid grid-two-cols">
-          <div className="login-image">
-            <img src="/images/login.png" alt="login img" 
-            width={"400"}
-            height={"400"	}/>
-          </div>
+      <main>
+        <div className="section-login">
+          <div className="container grid grid-two-cols">
+            <div className="login-image">
+              <img
+                src="/images/login.png"
+                alt="login img"
+                width={"400"}
+                height={"400"}
+              />
+            </div>
 
-          {/* Login form */}
-          <div className="Login-form">
-            <h1 className='main-heading mb-3'>Login Form</h1>
-            <br />
-
-            <form  onSubmit={handleSubmit}>
-              
-              <div>
-                <label htmlFor="email">Email</label>
-                <input 
-                type="email"
-                name='email'
-                id='email'
-                placeholder='Enter email'
-                required
-                autoComplete='off'
-                value={user.email}
-                onChange={handleInputs}
-                />
-              </div>
-              <div>
-                <label htmlFor="password">password</label>
-                <input 
-                type="password"
-                name='password'
-                id='password'
-                placeholder='Enter password'
-                required
-                autoComplete='off'
-                value={user.password}
-                onChange={handleInputs}
-                />
-              </div>
-
+            {/* Login form */}
+            <div className="login-form">
+              <h1 className="main-heading mb-3">Login Form</h1>
               <br />
-              <button type='submit' className='btn btn-submit'>Login Now</button>
-            </form>
 
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Enter email"
+                    required
+                    autoComplete="off"
+                    value={user.email}
+                    onChange={handleInputs}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Enter password"
+                    required
+                    autoComplete="off"
+                    value={user.password}
+                    onChange={handleInputs}
+                  />
+                </div>
+
+                <br />
+                <button type="submit" className="btn btn-submit">
+                  Login Now
+                </button>
+              </form>
+            </div>
           </div>
-
-
         </div>
-      </div>
-    </main>
-  </section>
-  )
-}
+      </main>
+    </section>
+  );
+};
 
-export default Login
+export default Login;
