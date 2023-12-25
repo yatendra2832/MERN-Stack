5c490f436378575c77b411b2984b7ff56d5ce553
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-25 22:13:22 +0530

    Connecting Frontend with the backend

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,109 +1,125 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
 
-const [user,setUser] = useState({
-  username:"",
-  email:"",
-  phone:"",
-  password:""
-})
+  // handling the input values
+  const handleInputs = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUser({ ...user, [name]: value });
+  };
 
-// handling the input values
-const handleInputs = (e) =>{
-  const name = e.target.name
-  const value = e.target.value
-  setUser({...user,[name]:value})
-}
-
-//Handling the form submission
-const handleSubmit = (e) =>{
-  e.preventDefault();
-  // console.log(user);
-}
-
+  //Handling the form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user);
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      if (response.ok) {
+        alert("Registration Successful");
+        setUser({ username: "", email: "", phone: "", password: "" });
+      }
+      console.log(response);
+    } catch (error) {
+      console.log("Register Error:", error);
+    }
+  };
   return (
     <section>
       <main>
         <div className="section-registration">
           <div className="container grid grid-two-cols">
             <div className="registration-image">
-              <img src="/images/register.png" alt="registration img" 
-              width={"400"}
-              height={"400"	}/>
+              <img
+                src="/images/register.png"
+                alt="registration img"
+                width={"400"}
+                height={"400"}
+              />
             </div>
 
             {/* registration form */}
             <div className="registration-form">
-              <h1 className='main-heading mb-3'>Registration Form</h1>
+              <h1 className="main-heading mb-3">Registration Form</h1>
               <br />
 
-              <form  onSubmit={handleSubmit}>
+              <form onSubmit={handleSubmit}>
                 <div>
                   <label htmlFor="username">Username</label>
-                  <input 
-                  type="text"
-                  name='username'
-                  id='username'
-                  placeholder='Enter Username'
-                  required
-                  autoComplete='off'
-                  value={user.username}
-                  onChange={handleInputs}
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Enter Username"
+                    required
+                    autoComplete="off"
+                    value={user.username}
+                    onChange={handleInputs}
                   />
                 </div>
                 <div>
                   <label htmlFor="email">Email</label>
-                  <input 
-                  type="email"
-                  name='email'
-                  id='email'
-                  placeholder='Enter email'
-                  required
-                  autoComplete='off'
-                  value={user.email}
-                  onChange={handleInputs}
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Enter email"
+                    required
+                    autoComplete="off"
+                    value={user.email}
+                    onChange={handleInputs}
                   />
                 </div>
                 <div>
                   <label htmlFor="phone">phone</label>
-                  <input 
-                  type="number"
-                  name='phone'
-                  id='phone'
-                  placeholder='Enter phone'
-                  required
-                  autoComplete='off'
-                  value={user.phone}
-                  onChange={handleInputs}
+                  <input
+                    type="number"
+                    name="phone"
+                    id="phone"
+                    placeholder="Enter phone"
+                    required
+                    autoComplete="off"
+                    value={user.phone}
+                    onChange={handleInputs}
                   />
                 </div>
                 <div>
                   <label htmlFor="password">password</label>
-                  <input 
-                  type="password"
-                  name='password'
-                  id='password'
-                  placeholder='Enter password'
-                  required
-                  autoComplete='off'
-                  value={user.password}
-                  onChange={handleInputs}
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Enter password"
+                    required
+                    autoComplete="off"
+                    value={user.password}
+                    onChange={handleInputs}
                   />
                 </div>
 
                 <br />
-                <button type='submit' className='btn btn-submit'>Register Now</button>
+                <button type="submit" className="btn btn-submit">
+                  Register Now
+                </button>
               </form>
-
             </div>
-
-
           </div>
         </div>
       </main>
     </section>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const authRoute = require('./router/auth-router');
 const contactRoute = require('./router/contact-router');
 const connectDB = require('./utils/db')
 const errorMiddleware = require('./middlewares/error-middleware');
 
+var corsOptions = {
+    origin: 'http://localhost:5173',
+    method: "GET, POST, PATCH, DELETE, PUT, HEAD",
+    credentials: true,
+}
+
 app.use(express.json());
+app.use(cors(corsOptions));
+
+
 
 app.use('/api/auth', authRoute);
 app.use('/api/form', contactRoute);
