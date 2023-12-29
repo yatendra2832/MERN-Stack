052b1e2ba07621132c3b33cb22b6a7e35b5f9aa3
Yatendra Singh <yatendrayadav2832@gmail.com>
2023-12-29 09:54:37 +0530

    admin contact feature added

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,5 +1,5 @@
 const User = require('../models/user-model');
-
+const Contact = require('../models/contact-model');
 const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find({}, { password: 0 });
@@ -13,4 +13,16 @@ const getAllUsers = async (req, res, next) => {
     }
 }
 
-module.exports = getAllUsers;
+const getAllContacts = async (req, res, next) => {
+    try {
+        const contacts = await Contact.find();
+        if (!contacts || contacts.length === 0) {
+            return res.status(404).json({ message: 'No contacts found' });
+        }
+        return res.status(200).json({ contacts });
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = { getAllUsers, getAllContacts };
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const getAllUsers = require('../controllers/admin-controller');
+const { getAllUsers } = require('../controllers/admin-controller');
+const { getAllContacts } = require('../controllers/admin-controller')
 
 router.route('/users').get(getAllUsers);
+router.route('/contacts').get(getAllContacts);
 
 module.exports = router;
